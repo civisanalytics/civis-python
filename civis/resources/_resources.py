04ab4d46caa165a6f0a898d023684a85624b44b6
Skipper Seabold <jsseabold@gmail.com>
2017-05-07 12:13:10 -0500

    COMPAT: ensure str

--- civis/resources/_resources.py
+++ civis/resources/_resources.py
@@ -245,7 +245,7 @@ def create_method(params, verb, method_name, path, doc):
     sig_self = create_signature(["self"] + sig_args, sig_kwargs)
     f.__signature__ = sig_self
     f.__doc__ = doc
-    f.__name__ = method_name
+    f.__name__ = str(method_name)
     return f
 
 
@@ -428,7 +428,7 @@ def parse_api_spec(api_spec, api_version, resources):
         class_name, methods = parse_path(path, ops, api_version, resources)
         class_name_lower = class_name.lower()
         if methods and classes.get(class_name_lower) is None:
-            classes[class_name_lower] = type(class_name, (Endpoint,), {})
+            classes[class_name_lower] = type(str(class_name), (Endpoint,), {})
         for method_name, method in methods:
             setattr(classes[class_name_lower], method_name, method)
     return classes
