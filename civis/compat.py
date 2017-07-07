93b18fd01d604e0190406211e05aec12d9d664fd
Stephen Hoover <shoover@civisanalytics.com>
2017-07-07 10:59:03 -0700

    [CIVP-11787] Add joblib backend (#106)
    * ENH Add Civis joblib backend
    
    Add a new `civis.parallel` module which contains tools to register a custom backend for the `joblib` library. This will let users leverage the Civis Platform infrastructure for parallel computing, and makes it easy to distribute parallel scikit-learn computations over the Civis infrastructure.
    
    Compared to development repo code:
    Change names to point to civis-python repo locations, add Python 2 compatibility, and tweak some docstrings.
    
    Instead of uploading the remote job runner function, pip install civis if needed.

--- civis/compat.py
+++ civis/compat.py
@@ -1,4 +1,5 @@
 # flake8: noqa
+from __future__ import print_function
 
 import six
 
@@ -15,3 +16,66 @@ else:
     from functools32 import lru_cache
     from funcsigs import signature
     FileNotFoundError = IOError
+
+if six.PY3:
+    from tempfile import TemporaryDirectory
+else:
+    # Backport TemporaryDirectory; this was introduced in Python 3.2
+    from tempfile import mkdtemp
+    import shutil as _shutil
+    import sys as _sys
+    import warnings as _warnings
+
+    class TemporaryDirectory(object):
+        """Create and return a temporary directory.  This has the same
+        behavior as mkdtemp but can be used as a context manager.  For
+        example:
+
+            with TemporaryDirectory() as tmpdir:
+                ...
+
+        Upon exiting the context, the directory and everything contained
+        in it are removed.
+
+        This is a port of the Python 3.2+ TemporaryDirectory object,
+        modified slightly to work with Python 2.7. Python 3 docs are at
+        https://docs.python.org/3/library/tempfile.html#tempfile.TemporaryDirectory
+        """
+
+        def __init__(self, suffix='', prefix='tmp', dir=None):
+            self._closed = False
+            self.name = None  # Handle mkdtemp raising an exception
+            self.name = mkdtemp(suffix, prefix, dir)
+
+        def __repr__(self):
+            return "<{} {!r}>".format(self.__class__.__name__, self.name)
+
+        def __enter__(self):
+            return self.name
+
+        def __exit__(self, exc, value, tb):
+            self.cleanup()
+
+        def __del__(self):
+            # Issue a ResourceWarning if implicit cleanup needed
+            self.cleanup(_warn=True)
+
+        def cleanup(self, _warn=False):
+            if self.name and not self._closed:
+                try:
+                    _shutil.rmtree(self.name)
+                except (TypeError, AttributeError) as ex:
+                    # Issue #10188: Emit a warning on stderr
+                    # if the directory could not be cleaned
+                    # up due to missing globals
+                    if "None" not in str(ex):
+                        raise
+                    print("ERROR: {!r} while cleaning up "
+                          "{!r}".format(ex, self,),
+                          file=_sys.stderr)
+                    return
+                self._closed = True
+                if _warn:
+                    _warnings.warn("Implicitly cleaning up {!r}".format(self),
+                                   ResourceWarning)
+
