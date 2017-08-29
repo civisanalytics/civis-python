8fd8793e33d35f36dbb94ddb41f12cb676fc7491
Stephen Hoover <shoover@civisanalytics.com>
2017-08-29 12:57:56 -0500

    ENH Bind optional arguments to endpoints by name
    Previously, optional arguments were all absorbed in a `**kwargs` blob and pulled out later. This changes to set all of them explicitly in the function signature. Doing this improves the readability of the docstrings, and also allows other code (e.g. `mock`'s autospeccer) to read expected function signatures. Note that the actual function code, in `resources._resources.create_method` remains the same. It takes `*args, **kwargs` inputs. The function's signature gets patched after it's created. Because the code remains the same, we never actually transmit default values to the API.
    
    I've set default values to the string "DEFAULT" in an attempt to indicate to users that the defaults in the function signature are different from the defaults in the API.
    
    I'm running `raise_for_unexpected_kwargs` only on keyword arguments. `sig.bind` will only give one unexpected keyword argument even if many are present, so I've preserved our own error handling for that. `sig.bind` will take care of errors for positional arguments and collisions of positional arguments and keyword arguments.
    
    In Python 2, optional arguments will now be able to be given positionally. The previous, keyword-only behavior, is preserved in Python 3.
    
    I've deleted one test from `test_resources` which fails now; it doesn't seem like it should have ever passed. `iterator` wasn't in the spec, so trying to call it should have made an error.

--- civis/tests/test_resources.py
+++ civis/tests/test_resources.py
@@ -42,20 +42,6 @@ updated_at : string/time
     The last modification time for this credential.""")  # noqa: E122
 
 
-def test_create_method_iterator_kwarg():
-    args = [{"name": 'limit', "in": 'query', "required": False, "doc": ""},
-            {"name": 'page_num', "in": 'query', "required": False, "doc": ""},
-            {"name": 'order', "in": 'query', "required": False, "doc": ""},
-            {"name": 'order_by', "in": 'query', "required": False, "doc": ""}]
-    method = _resources.create_method(args, 'get', 'mock_name', '/objects',
-                                      'fake_doc')
-    mock_endpoint = mock.MagicMock()
-
-    method(mock_endpoint, iterator=True)
-    mock_endpoint._call_api.assert_called_once_with(
-        'get', '/objects', {}, {}, iterator=True)
-
-
 def test_create_method_no_iterator_kwarg():
 
     # Test that dynamically-created function errors when an
@@ -231,12 +217,17 @@ def test_expired_api_key(mock_response):
     assert http_error_raised
 
 
-def test_create_method_unexpected_kwargs():
+def _create_mock_endpoint():
     args = [{"name": 'foo', "in": 'query', "required": True, "doc": ""},
             {"name": 'bar', "in": 'query', "required": False, "doc": ""}]
     method = _resources.create_method(args, 'get', 'mock_name', '/objects',
                                       'fake_doc')
     mock_endpoint = mock.MagicMock()
+    return mock_endpoint, method
+
+
+def test_create_method_unexpected_kwargs():
+    mock_endpoint, method = _create_mock_endpoint()
 
     # Method works without unexpected kwarg
     method(mock_endpoint, foo=0, bar=0)
@@ -253,6 +244,36 @@ def test_create_method_unexpected_kwargs():
     with pytest.raises(TypeError) as excinfo:
         method(mock_endpoint, foo=0, bar=0, baz=0)
     assert str(excinfo.value) == expected_msg
+
+
+def test_create_method_too_many_pos_args():
+    mock_endpoint, method = _create_mock_endpoint()
+
+    # Method raises TypeError with too many arguments
+    with pytest.raises(TypeError) as excinfo:
+        method(mock_endpoint, 0, 0, 0)
+    assert str(excinfo.value) == "too many positional arguments"
+
+
+def test_create_method_multiple_values():
+    mock_endpoint, method = _create_mock_endpoint()
+
+    # Method raises TypeError with multiple values for arguments
+    with pytest.raises(TypeError) as excinfo:
+        method(mock_endpoint, 0, foo=0)
+    assert str(excinfo.value) == "multiple values for argument 'foo'"
+
+
+@pytest.mark.skipif(six.PY2, reason='Keyword-only parameters are '
+                                    'not in Python 2')
+def test_create_method_keyword_only():
+    # Verify that optional arguments are keyword-only
+    # (This language feature is only present in Python 3)
+    mock_endpoint, method = _create_mock_endpoint()
+
+    with pytest.raises(TypeError) as excinfo:
+        method(mock_endpoint, 0, 0)
+    assert str(excinfo.value) == "too many positional arguments"
 
 
 @mock.patch(MOCKED_OPEN, new_callable=mock.mock_open,
