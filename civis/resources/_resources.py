310979570731976e6e353d3003b65c7cbc0818fb
Michael Heilman <mheilman@civisanalytics.com>
2017-10-13 10:52:06 -0500

    style change to resource list

--- civis/resources/_resources.py
+++ civis/resources/_resources.py
@@ -19,9 +19,22 @@ from civis._utils import (camel_to_snake, to_camelcase,
 
 
 API_VERSIONS = ["1.0"]
-BASE_RESOURCES_V1 = ["credentials", "databases", "files", "imports",
-                     "jobs", "models", "predictions", "projects",
-                     "queries", "reports", "scripts", "tables", "users"]
+BASE_RESOURCES_V1 = [
+    "credentials",
+    "databases",
+    "files",
+    "imports",
+    "jobs",
+    "models",
+    "predictions",
+    "projects",
+    "queries",
+    "reports",
+    "scripts",
+    "tables",
+    "users"
+]
+
 TYPE_MAP = {"array": "list", "object": "dict"}
 ITERATOR_PARAM_DESC = (
     "iterator : bool, optional\n"
