80b9a3fe4a979baacfc13b53575bdb1572ae640c
jacksonllee <jacksonlee@civisanalytics.com>
2018-02-15 22:04:44 -0600

    DOC Not using submodules in building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,16 +13,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
 import os
-import shlex
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../sphinxext'))
-themedir = os.path.join(os.pardir, 'sphinx_rtd_theme')
-sys.path.insert(0, themedir)
 
 # -- General configuration ------------------------------------------------
 
