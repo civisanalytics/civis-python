5753d107de4faf3e0e760dcfa3cedda769311300
jacksonllee <jacksonlee@civisanalytics.com>
2019-02-19 15:50:52 -0600

    TST mock_table as a more reasonable var name

--- civis/tests/test_civis.py
+++ civis/tests/test_civis.py
@@ -13,10 +13,10 @@ def test_get_table_id(schema_tablename):
     mock_client = mock.Mock()
     mock_client.get_database_id.return_value = 123
 
-    mock_tables = mock.MagicMock()
-    mock_tables.__getitem__.side_effect = {0: mock.Mock()}.__getitem__
+    mock_table = mock.MagicMock()
+    mock_table.__getitem__.side_effect = {0: mock.Mock()}.__getitem__
 
-    mock_client.tables.list.return_value = [mock_tables]
+    mock_client.tables.list.return_value = [mock_table]
 
     civis.APIClient.get_table_id(mock_client,
                                  table=schema_tablename, database=123)
