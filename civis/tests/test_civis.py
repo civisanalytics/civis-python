2cef411f28d6257d17504add8885516150867aff
jacksonllee <jacksonlunlee@gmail.com>
2019-02-19 20:27:18 -0600

    TST use TEST_SPEC to make a real client object

--- civis/tests/test_civis.py
+++ civis/tests/test_civis.py
@@ -1,5 +1,6 @@
 import civis
 from civis.compat import mock
+from civis.tests.mocks import TEST_SPEC
 
 import pytest
 
@@ -9,18 +10,17 @@ import pytest
 ])
 def test_get_table_id(schema_tablename):
     """Check that get_table_id handles quoted schema.tablename correctly."""
-    mock_client = mock.Mock()
-    mock_client.get_database_id.return_value = 123
+    client = civis.APIClient(local_api_spec=TEST_SPEC, api_key='none')
+    client.get_database_id = mock.Mock(return_value=123)
 
     mock_tables = mock.MagicMock()
     mock_tables.__getitem__.side_effect = {0: mock.Mock()}.__getitem__
 
-    mock_client.tables.list.return_value = mock_tables
+    client.tables.list = mock.Mock(return_value=mock_tables)
 
-    civis.APIClient.get_table_id(mock_client,
-                                 table=schema_tablename, database=123)
+    client.get_table_id(table=schema_tablename, database=123)
 
-    mock_client.tables.list.assert_called_once_with(
+    client.tables.list.assert_called_once_with(
         database_id=123,
         schema='foo',
         name='bar'
