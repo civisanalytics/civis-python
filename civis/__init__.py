d80a35a1c5680419547c4009dd67ba123a7d3ed1
Stephen Hoover <shoover@civisanalytics.com>
2019-06-04 12:57:18 -0500

    ENH utils module available from civis namespace
    Import `utils` into `civis` so that users can access `civis.utils.run_job` after an `import civis` import. Many users expect this and are surprised when it's not available.

--- civis/__init__.py
+++ civis/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from civis._version import __version__
 from civis.civis import APIClient, find, find_one
-from civis import io, ml, parallel
+from civis import io, ml, parallel, utils
 
 __all__ = ["__version__", "APIClient", "find", "find_one", "io",
-           "ml", "parallel"]
+           "ml", "parallel", "utils"]
