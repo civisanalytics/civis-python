0f6c2a1a73d1affd17f4ba3143df95717e36709c
Liz Sander <lsander@civisanalytics.com>
2019-08-02 13:51:57 -0500

    STY remove stray constants

--- civis/ml/tests/test_helper.py
+++ civis/ml/tests/test_helper.py
@@ -4,9 +4,6 @@ import pytest
 from civis.response import Response
 from civis.ml import list_models
 
-TRAIN_TEMPLATES = '11219, 11221, 10582, 9968, 9112, 8387, 7020'
-PRED_TEMPLATES = '11220, 10583, 9969, 9113, 8388, 7021'
-
 
 def test_list_models_bad_job_type():
     with pytest.raises(ValueError):
