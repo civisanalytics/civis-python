372784a271e14fa28393bc980f813b1a9ec52d63
Liz Sander <lsander@civisanalytics.com>
2020-04-09 12:57:41 -0500

    DOC full path for exception

--- civis/futures.py
+++ civis/futures.py
@@ -235,7 +235,7 @@ class CivisFuture(PollableResult):
 
         Raises
         ------
-        CivisJobFailure
+        civis.base.CivisJobFailure
             If the job fails.
         """
         self.result()
