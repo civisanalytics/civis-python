28db72c7c8ec3e4e14e3fa9fd6665b70c91f550c
Izzy Zelichenko <izelichenko@civisanalytics.com>
2020-08-14 14:23:51 -0500

    - Shorten line (flake8 error)

--- civis/io/_tables.py
+++ civis/io/_tables.py
@@ -1037,7 +1037,8 @@ def civis_file_to_table(file_id, database, table, client=None,
                 break
 
     need_table_columns = (((not table_exists or existing_table_rows == 'drop')
-                          and table_columns is None) or not table_columns_have_sql)
+                          and table_columns is None)
+                          or not table_columns_have_sql)
 
     cleaning_futures = _run_cleaning(file_id, client, need_table_columns,
                                      headers, delimiter, hidden)
