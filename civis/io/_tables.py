e93b467faefcbd5f6fd02529fabb17b0416d99ae
Izzy Zelichenko <izelichenko@civisanalytics.com>
2020-08-14 14:10:16 -0500

    - If table_columns is provided, assert that teach column has a sql_type. Otherwise, perform cleaning process. - Add test for this case

--- civis/io/_tables.py
+++ civis/io/_tables.py
@@ -1029,8 +1029,15 @@ def civis_file_to_table(file_id, database, table, client=None,
 
     # Use Preprocess endpoint to get the table columns as needed
     # and perform necessary file cleaning
-    need_table_columns = ((not table_exists or existing_table_rows == 'drop')
-                          and table_columns is None)
+    table_columns_have_sql = True
+    if table_columns:
+        for col in table_columns:
+            if not col['sql_type']:
+                table_columns_have_sql = False
+                break
+
+    need_table_columns = (((not table_exists or existing_table_rows == 'drop')
+                          and table_columns is None) or not table_columns_have_sql)
 
     cleaning_futures = _run_cleaning(file_id, client, need_table_columns,
                                      headers, delimiter, hidden)
