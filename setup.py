f9b9ea5f22ccaac027dba7cea1c00597961d20ce
Michael Heilman <mheilman@civisanalytics.com>
2020-09-08 16:55:16 -0500

    remove python 3.5 support since it is reaching EOL (cf. https://www.python.org/downloads/release/python-3510/)

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from setuptools import find_packages, setup
 CLASSIFIERS = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
