0ff013793922665a4720c6449496614a452f3588
jacksonlee-civis <jacksonlee@civisanalytics.com>
2021-11-30 08:03:03 -0500

    FIX set loosen_types to True even only one file id

--- civis/io/_tables.py
+++ civis/io/_tables.py
@@ -1101,12 +1101,12 @@ def civis_file_to_table(file_id, database, table, client=None,
     redshift_options = dict(distkey=distkey, sortkeys=[sortkey1, sortkey2],
                             diststyle=diststyle)
 
-    # If multiple files are being imported and the user hasn't explicitly
+    # If the user hasn't explicitly
     # provided table column info, then there might be differences in
     # their precisions/lengths - setting this option will allow the Civis API
     # to increase these values for the data types provided, and decreases the
     # risk of a length-related import failure when types are inferred.
-    loosen_types = need_table_columns and len(file_id) > 1
+    loosen_types = need_table_columns
 
     import_name = 'CSV import to {}.{}'.format(schema, table_name)
     import_job = client.imports.post_files_csv(
