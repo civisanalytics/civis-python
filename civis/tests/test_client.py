f02ede4c2011cba00495cda21c2a5704c62e7f74
Gregory Karpov <gkarpov@civisanalytics.com>
2023-02-03 13:24:30 -0500

    Remove CivisVCRTestCase.

--- civis/tests/test_client.py
+++ civis/tests/test_client.py
@@ -2,8 +2,6 @@ from unittest import mock
 
 from civis import APIClient
 from civis.resources import API_SPEC
-from civis.resources._resources import get_api_spec, generate_classes
-from civis.tests.testcase import CivisVCRTestCase
 
 api_import_str = 'civis.resources._resources.get_api_spec'
 
@@ -30,25 +28,3 @@ def test_feature_flags_memoized(mock_spec):
         client.feature_flags
         client.feature_flags
         assert client.users.list_me.call_count == 1
-
-
-class ClientTests(CivisVCRTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        get_api_spec.cache_clear()
-        generate_classes.cache_clear()
-
-    @classmethod
-    def tearDownClass(cls):
-        get_api_spec.cache_clear()
-        generate_classes.cache_clear()
-
-    @mock.patch(api_import_str, return_value=API_SPEC)
-    def test_feature_flags_memoized2(self, *mocks):
-        client = APIClient()
-        with mock.patch.object(client.users, 'list_me',
-                               wraps=client.users.list_me):
-            client.feature_flags
-            client.feature_flags
-            self.assertEqual(client.users.list_me.call_count, 1)
