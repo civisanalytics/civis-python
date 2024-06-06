6c00fc4928c1820a75480f1a1b0efef6e638b08f
jacksonlee-civis <jacksonlee@civisanalytics.com>
2024-06-06 14:54:07 -0400

    PERF import top-level modules lazily

--- src/civis/__init__.py
+++ src/civis/__init__.py
@@ -1,11 +1,28 @@
+import importlib
+import sys
 from importlib.metadata import version
 
 from civis.client import APIClient
 from civis.loggers import civis_logger
 from civis.response import find, find_one
 from civis.service_client import ServiceClient
-from civis import io, ml, parallel, utils
 
+
+def _lazy_import(name):
+    # https://docs.python.org/3/library/importlib.html#implementing-lazy-imports
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
+
+
+io = _lazy_import("civis.io")
+ml = _lazy_import("civis.ml")
+parallel = _lazy_import("civis.parallel")
+utils = _lazy_import("civis.utils")
 
 __version__ = version("civis")
 __all__ = [
